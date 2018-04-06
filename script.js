7e56b57733acabad350bfbce5dcf5ab108854b46
vidyak2 <vidyakrishnan.vk@gmail.com>
2018-04-05 19:32:55 -0700

    Adding courses almost functional

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ var courseList = [];
 // REVIEW: Would this be safer if variable was scoped to window.onload or addACourse functions?
 
 // Assign handlers to page events.
-window.onload = function(){
+window.onload = function () {
+
   document.querySelector("form#add-course").addEventListener("submit", addACourse)
 
   document.querySelector("button#clear").addEventListener("click", clearData)
@@ -33,11 +34,23 @@ window.onload = function(){
 // Triggered on form submit. Creates a new course object and pushes it into courseList array,
 // clears content in form fields, prints courseList objects to the page.
 // BUGFIX: This function isn't working properly- nothing gets output to the list on form submit
-function addACourse(){
-  var grade = parseFloat(this.elements["grade"].value);
-  /*
-    TODO: validate that "grade" value is a number between 1.0 and 4.0, stop processing if it is not.
+function addACourse() {
+  //When the answer and the console log keeps refreshing and you can't see in the console, tht means you forgot event.preventDefault
+  event.preventDefault()
 
+  //The elements with the value of grade at taken from the form
+  var grade = parseFloat(this.elements["grade"].value);
+  console.log("grade: ", grade)
+
+  // TODO: validate that "grade" value is a number between 1.0 and 4.0, stop processing if it is not.
+  //  if (grade > 0 && grade < 4) {
+  //    console.log("Thank you!")
+  //  } else {
+  //    console.log("This grade is invalid")
+  //  }
+
+
+  /*
     Checking 'grade typeof "number"' will always return true because we called parseFloat.
     We must instead check that it's value is not NaN.
 
@@ -45,12 +58,14 @@ function addACourse(){
   */
 
   // Create the new course with values from the form, push it into array of courses.
+  //Creating an object
   var newCourse = {
     name: this.elements["name"].value,
     grade: grade
   }
-
+  //Push the new object into the array of courses
   courseList.push(newCourse)
+  console.log("courseList: ", courseList)
 
   clearFormFields()
   outputList()
@@ -58,11 +73,11 @@ function addACourse(){
 
 // Calculate the average of "grade" attribute for all objects in courseList array
 // and print it in friendly message to page text.
-function calculateAverage(){
+function calculateAverage() {
   // Average GPA of courses equals the sum of all grades,
   // divided by the number of course objects in courseList
   var sum = 0
-  for(var i=0;i<courseList.length;i++){
+  for (var i = 0; i < courseList.length; i++) {
     sum += courseList[i].grade
   }
   var avg = sum / courseList.length
@@ -70,28 +85,45 @@ function calculateAverage(){
 }
 
 // Removes GPA calculation from page text.
-function clearGPA(){
+function clearGPA() {
   document.getElementById("result").innerHTML = null;
 }
 
 // Clears content in form fields.
-function clearFormFields(){
+//Good user experience
+function clearFormFields() {
   // TODO: implement this function.
 }
 
 // Clear out list of courses and all content shown on the page
-function clearData(){
+function clearData() {
   courseList = []
   clearFormFields()
   outputList()
 }
 
 // Prints courseList objects to the page in a readable way.
-function outputList(){
+function outputList() {
   var list = document.getElementById("course-list");
+  console.log("list: ", list)
+
   /*
     TODO: Clear the existing contents of the "list" element. Then, for each object in courseList,
     create an li element that holds the course's name and grade, and append
     it to the "list" ul element.
   */
+  //We want to clear out all the elements, and then add li elements to create a list
+  list.innerHTML = ""
+
+  for (var i = 0; i < courseList.length; i++) {
+    console.log("name: ", courseList[i].name, "grade: ", courseList[i].grade)
+  }
+
+
+  //THIS DOES NOT WORK
+  var newLi = document.createElement("li")
+  newLi.innerHTML = "name: " + courseList[i].name + " grade: " + courseList[i].grade
+
+  list.appendChild(newLi)
+
 }
